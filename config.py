649f3efc6f3b2dfd25027f7782b6ec23da932620
Jerry Wu <wu0952686160@gmail.com>
2026-06-22 15:58:13 +0800

    Prepare for Render deployment

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+import os
 class Config:
-    SECRET_KEY = 'bike-train-secret-key'
 
-    SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://root:950822@localhost/biketrain'
+    SECRET_KEY = os.environ.get(
+        'SECRET_KEY',
+        'bike-train-secret-key'
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL'
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
